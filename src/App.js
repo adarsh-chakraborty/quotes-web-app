c5d3c16a903f2ac40c1bebd9e645afbc013bedc9
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-11-17 13:54:18 +0530

    Added Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 
+import AllQuotes from './components/pages/AllQuotes.js';
+import NewQuote from './components/pages/NewQuote.js';
+import QuoteDetail from './components/pages/QuoteDetail.js';
+
 function App() {
-  return (
-    <Routes>
-      <route path="/quotes" element={ }/>
-      <route path="/quotes/:quoteId" element={ }/>
-      <route path="/new-quote" element={ }/>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/quotes" element={<AllQuotes />} />
+			<Route path="/quotes/:quoteId" element={<QuoteDetail />} />
+			<Route path="/new-quote" element={<NewQuote />} />
+			<Route path="/*" element={<AllQuotes />} />
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/components/pages/NewQuote.js
@@ -0,0 +1,5 @@
+const NewQuote = () => {
+	return <h1>Add a new Quote</h1>;
+};
+
+export default NewQuote;
