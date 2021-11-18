03a09b3e739304fa598071cf4bdb96731df05fd0
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-11-18 15:09:28 +0530

    Added Lazy Loading && Ready to Deploy

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Great Quotes
+
+Write and share your quotes. Just a Minor React Project
+
+### Build status
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/163fdd91-5eb9-44b7-9ff6-6043c5a4999d/deploy-status)](https://app.netlify.com/sites/somegreatquotes/deploys)
+
+### Url
+
+- https://somegreatquotes.netlify.app
+
+### Deployment Logs
+
+- https://app.netlify.com/sites/somegreatquotes/deploys
--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
+import React, { Suspense } from 'react';
+
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import AllQuotes from './components/pages/AllQuotes.js';
-import NewQuote from './components/pages/NewQuote.js';
+
 import QuoteDetail from './components/pages/QuoteDetail.js';
 import Comments from './components/comments/Comments.js';
 import Layout from './components/layout/Layout';
 import NotFound from './components/pages/NotFound.js';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./components/pages/NewQuote.js'));
 
 function App() {
 	return (
 		<Layout>
-			<Routes>
-				<Route path="/" element={<Navigate to="/quotes" />} />
-				<Route path="/quotes">
-					<Route index element={<AllQuotes />} />
+			<Suspense
+				fallback={
+					<div className="centered">
+						<LoadingSpinner />
+					</div>
+				}
+			>
+				<Routes>
+					<Route path="/" element={<Navigate to="/quotes" />} />
+					<Route path="/quotes">
+						<Route index element={<AllQuotes />} />
 
-					<Route path=":quoteId" element={<QuoteDetail />}>
-						<Route path="comments" element={<Comments />} />
+						<Route path=":quoteId" element={<QuoteDetail />}>
+							<Route path="comments" element={<Comments />} />
+						</Route>
 					</Route>
-				</Route>
 
-				<Route path="/new-quote" element={<NewQuote />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+					<Route path="/new-quote" element={<NewQuote />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Suspense>
 		</Layout>
 	);
 }
